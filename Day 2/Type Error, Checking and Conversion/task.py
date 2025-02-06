1641cbae08c7c497008b754234db7da494e68a63
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2025-02-06 15:22:45 +0300

    day 2 task 2 finished

--- Day 2/Type Error, Checking and Conversion/task.py
+++ Day 2/Type Error, Checking and Conversion/task.py
@@ -1,1 +1,13 @@
-len(12345)
+print(len("12345"))
+
+print(type("12345"))
+print(type(123))
+print(type(True))
+print(type(123.45))
+
+print(int("123") +int("456"))
+
+name_of_the_user = input("Enter your name")
+length_of_name = len(name_of_the_user)
+
+print("Number of letters in your name: " + str(length_of_name))
