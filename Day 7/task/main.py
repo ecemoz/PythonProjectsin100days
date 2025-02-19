575beb7c7f5987de32f3b640b9d7f9694b575b64
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2025-02-19 17:38:36 +0300

    day 7 task 4 finished

--- Day 7/task/main.py
+++ Day 7/task/main.py
@@ -1,5 +1,66 @@
 import random
+stages = [r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 word_list = ["aardvark", "baboon", "camel"]
+
+# TODO-1: - Create a variable called 'lives' to keep track of the number of lives left.
+#  Set 'lives' to equal 6.
+
+lives = 6
 
 chosen_word = random.choice(word_list)
 print(chosen_word)
@@ -10,7 +71,6 @@ for position in range(word_length):
     placeholder += "_"
 print(placeholder)
 
-# TODO-1: - Use a while loop to let the user guess again.
 game_over = False
 correct_letters = []
 
@@ -18,8 +78,6 @@ while not game_over:
     guess = input("Guess a letter: ").lower()
 
     display = ""
-
-# TODO-2: Change the for loop so that you keep the previous correct letters in display.
 
     for letter in chosen_word:
         if letter == guess:
@@ -29,9 +87,27 @@ while not game_over:
             display += letter
         else:
             display += "_"
+            lives -= 1
 
     print(display)
 
+    # TODO-2: - If guess is not a letter in the chosen_word, Then reduce 'lives' by 1.
+    #  If lives goes down to 0 then the game should stop and it should print "You lose."
+
+    if guess not in chosen_word:
+        lives -= 1
+
     if "_" not in display:
         game_over = True
-        print("you win.")
+        print("You win.")
+
+    # TODO-3: - print the ASCII art from 'stages'
+    #  that corresponds to the current number of 'lives' the user has remaining.
+
+    if lives == 0:
+        game_over = True
+        print("You Lose!")
+
+    i = lives - 7
+    ascii = stages[i]
+    print(ascii)
