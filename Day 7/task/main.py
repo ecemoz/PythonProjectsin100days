d50eaf52669a8357239f9668d2099fdfa479a464
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2025-02-19 15:58:46 +0300

    day 7 task 1 finished

--- Day 7/task/main.py
+++ Day 7/task/main.py
@@ -1,3 +1,4 @@
+import random
 word_list = ["aardvark", "baboon", "camel"]
 
 # TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word. Then print it.
@@ -6,3 +7,16 @@ word_list = ["aardvark", "baboon", "camel"]
 
 # TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word. Print "Right" if it
 #  is, "Wrong" if it's not.
+
+chosen_word = random.choice(word_list)
+print(chosen_word)
+
+guest = input("Please guess a letter").lower()
+chosen_word_letters = list(chosen_word)
+print(chosen_word_letters)
+
+for letter in chosen_word_letters:
+    if letter == guest:
+        print(letter, end="")
+    else:
+        print("_", end="")
