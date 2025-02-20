7d2ad63988bfea6fa05f1b53499c67838bc1ced3
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2025-02-20 17:16:19 +0300

    day 8 task 2 finished

--- Day 8/Positional vs Keyword Arguments/task.py
+++ Day 8/Positional vs Keyword Arguments/task.py
@@ -1,8 +1,7 @@
 # Functions with input
 
-def greet_with_name(name):
+def greet_with(name, location):
     print(f"Hello {name}")
-    print(f"How do you do {name}?")
+    print(f"What is it like in {location}")
 
-
-greet_with_name("Jack Bauer")
+greet_with("Emre", "my heart")
