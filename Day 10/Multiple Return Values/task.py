e65b93fdcd76e0b345ea864176a349fb8d033c95
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2025-02-22 15:09:27 +0300

    day 10 task 3 finished.

--- Day 10/Multiple Return Values/task.py
+++ Day 10/Multiple Return Values/task.py
@@ -5,5 +5,4 @@ def format_name(f_name, l_name):
     formated_l_name = l_name.title()
     return f"{formated_f_name} {formated_l_name}"
 
-
 print(format_name(input("What is your name?"), input("What is your last name")))
